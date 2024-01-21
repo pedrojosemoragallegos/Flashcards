086d58b25b10c4614888cfce0047e82029bdac86
Pedro José <pedrojosemoragallegos@outlook.de>
2024-01-21 13:16:40 +0100

    corrected

--- Flashcards/SpacedRepetition/LeitnerSystem.swift
+++ Flashcards/SpacedRepetition/LeitnerSystem.swift
@@ -18,7 +18,7 @@ struct LeitnerSystem: SpacedRepetitionProtocol {
         }
     }
     
-    func updateFlashcardBox(card: Flashcard, isCorrect: Bool) {
+    /*func updateFlashcardBox(card: Flashcard, isCorrect: Bool) {
         guard let currentBox = boxMapping.first(where: { $0.value.contains(where: { $0.question == card.question && $0.answer == card.answer }) })?.key else { return }
 
         boxMapping[currentBox]?.removeAll(where: { $0.question == card.question && $0.answer == card.answer })
@@ -26,5 +26,6 @@ struct LeitnerSystem: SpacedRepetitionProtocol {
         let newBox = isCorrect ? min(currentBox + 1, numberOfBoxes) : 1
         boxMapping[newBox, default: []].append(card)
     }
+     */w
     
 }
